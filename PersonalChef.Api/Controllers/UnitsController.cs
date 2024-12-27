f386ea2741a3d9e6950575109e11064af4f008dc
Dave Hynek <david.alan.hynek@gmail.com>
2024-12-27 13:19:38 -0500

    Add Units Controller
    Basic Controller for retrieving valid units.

---
+++ PersonalChef.Api/Controllers/UnitsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace PersonalChef.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnitsController : ControllerBase
+    {
+        private readonly IDbContextFactory<RecipeContext> _contextFactory;
+
+        public UnitsController(IDbContextFactory<RecipeContext> contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            await using var context = _contextFactory.CreateDbContext();
+
+            return await context.MeasurementUnits
+                .Select(x => x.DisplayName)
+                .OfType<string>()
+                .ToListAsync();
+        }
+    }
+}
